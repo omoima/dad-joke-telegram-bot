5e3a4455c0cda2bed48036a017078649e3433db9
Oarabile Moima <oarabile@bbd.co.za>
2022-08-19 16:20:42 +0200

    Use AssertEqual as recommended

--- test_input.py
+++ test_input.py
@@ -5,23 +5,23 @@ class MessageTest(unittest.TestCase):
 
     def test_hi(self):
         result = message("hi")
-        self.assertEquals(False, result)
+        self.assertEqual(False, result)
 
     def test_Hello(self):
         result = message("Hello")
-        self.assertEquals(True, result)
+        self.assertEqual(True, result)
 
     def test_hello(self):
         result = message("hello")
-        self.assertEquals(True, result)
+        self.assertEqual(True, result)
 
     def test_heLLo(self):
         result = message("heLLo")
-        self.assertEquals(True, result)
+        self.assertEqual(True, result)
 
     def test_gello(self):
         result = message("gello")
-        self.assertEquals(False, result)
+        self.assertEqual(False, result)
 
 
 if __name__=="__main__":
